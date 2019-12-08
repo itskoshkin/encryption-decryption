4e31ee640366b401acc1139d1014aac92cd3ea62
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2019-12-08 16:15:17 +0300

    wrote logic for Application

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,78 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import encdec.Shift;
+import encdec.Unicode;
+import input.ConsoleInput;
+import input.FileInput;
+
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
-        for (int i = 0; i < args.length; i += 2) {
+        for (int i = 0; i < args.length; i += 2)
             hashMap.put(args[i], args[i + 1]);
-        }
+        new Main().run(hashMap);
+    }
 
+    private void run(HashMap<String, String> hashMap) {
         if (hashMap.containsKey("-data")) {
-            System.out.print(new Main().encdec(
-                    hashMap.get("-mode"),
-                    hashMap.get("-data").replace("\"", ""),
-                    Integer.parseInt(hashMap.get("-key"))));
+            runWithData(hashMap);
         } else {
-            new Main().encdec(
-                    hashMap.get("-mode"),
-                    hashMap.get("-in"),
-                    hashMap.get("-out"),
-                    Integer.parseInt(hashMap.get("-key")));
-        }
-
-
-    }
-
-
-    private char[] encdec(String func, String string, int key) {
-        char[] character = new char[string.length()];
-        if ("enc".equals(func)) {
-            for (int i = 0; i < string.length(); i++)
-                character[i] = (char) (string.charAt(i) + key);
-            return character;
-        } else if ("dec".equals(func)) {
-            for (int i = 0; i < string.length(); i++)
-                character[i] = (char) (string.charAt(i) - key);
-            return character;
-        } else {
-            throw new Error();
+            runWithFile(hashMap);
         }
     }
 
-
-    private void encdec(String func, String in, String out, int key) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(in))) {
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(bufferedReader.readLine());
-            char[] character = new char[stringBuilder.length()];
-            if ("enc".equals(func)) {
-                for (int i = 0; i < stringBuilder.length(); i++)
-                    character[i] = (char) (stringBuilder.charAt(i) + key);
-                try (FileWriter writer = new FileWriter(out, false)) {
-                    writer.write(character);
-                }
-            } else if ("dec".equals(func)) {
-                for (int i = 0; i < stringBuilder.length(); i++)
-                    character[i] = (char) (stringBuilder.charAt(i) - key);
-                try (FileWriter writer = new FileWriter(out, false)) {
-                    writer.write(character);
-                }
+    private void runWithFile(HashMap<String, String> hashMap) {
+        FileInput fileInput = new FileInput(hashMap.get("-in"), hashMap.get("-out"));
+        char[] chars = fileInput.input();
+        if ("unicode".equals(hashMap.get("-alg"))) {
+            if ("enc".equals(hashMap.get("-mode"))) {
+                Unicode unicode = new Unicode(chars);
+                unicode.encrypt(Integer.parseInt(hashMap.get("-key")));
+                fileInput.output(unicode.getChars());
+            } else {
+                Unicode unicode = new Unicode(chars);
+                unicode.decrypt(Integer.parseInt(hashMap.get("-key")));
+                fileInput.output(unicode.getChars());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            if ("enc".equals(hashMap.get("-mode"))) {
+                Shift shift = new Shift(chars);
+                shift.encrypt(Integer.parseInt(hashMap.get("-key")));
+                fileInput.output(shift.getChars());
+            } else {
+                Shift shift = new Shift(chars);
+                shift.decrypt(Integer.parseInt(hashMap.get("-key")));
+                fileInput.output(shift.getChars());
+            }
         }
     }
+
+    private void runWithData(HashMap<String, String> hashMap) {
+        ConsoleInput consoleInput = new ConsoleInput(hashMap.get("-data"));
+        char[] chars = consoleInput.input();
+        if ("unicode".equals(hashMap.get("-alg"))) {
+            if ("enc".equals(hashMap.get("-mode"))) {
+                Unicode unicode = new Unicode(chars);
+                unicode.encrypt(Integer.parseInt(hashMap.get("-key")));
+                System.err.print(unicode.getChars());
+            } else {
+                Unicode unicode = new Unicode(chars);
+                unicode.decrypt(Integer.parseInt(hashMap.get("-key")));
+                System.out.print(unicode.getChars());
+            }
+        } else {
+            if ("enc".equals(hashMap.get("-mode"))) {
+                Shift shift = new Shift(chars);
+                shift.encrypt(Integer.parseInt(hashMap.get("-key")));
+                System.out.print(shift.getChars());
+            } else {
+                Shift shift = new Shift(chars);
+                shift.decrypt(Integer.parseInt(hashMap.get("-key")));
+                System.out.print(shift.getChars());
+            }
+        }
+    }
+
 
 }
